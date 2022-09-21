3ba14430744a3b04ed1646adfd390a56321895c1
Kee <shikee.maloney@codeimmersives.com>
2022-09-21 16:35:21 -0400

    99

---
+++ week-one/99-bottles.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')({sigint: true})
+
+howMany = prompt('HOW MANY BOTTLES OF BEER ON THE WALL!! ')
+
+beer = ''
+
+oneBeer = 1
+
+noBeer = 0
+
+console.log(`${howMany} bottles of beer on the wall, ${howMany} bottles of beer, take one down pass it around ${howMany - 1} bottles of beer on the wall.`)
+while (howMany>noBeer){
+  howMany = howMany - oneBeer
+  if(howMany>oneBeer){
+  console.log(`${howMany} bottles of beer on the wall, ${howMany} bottles of beer, take one down pass it around ${howMany - 1} bottles of beer on the wall.`)
+  
+  }else{
+    console.log(`${oneBeer} bottle of beer on the wall, ${oneBeer} bottle of beer, take one down pass it aroud, no more bottles of beer on the wall. Go to the store buy some more, 99 bottles of beer on the wall!`)
+  }  
+}
